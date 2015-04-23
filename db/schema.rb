6b35efae1bf514c4a75aac3bfa43372ccff0cce2
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-23 16:25:55 -0400

    Added restaurant and user models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423185800) do
+ActiveRecord::Schema.define(version: 20150423201026) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150423185800) do
     t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
