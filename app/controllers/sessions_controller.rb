87609204e773cf5c8a310c562e436efe84f1ce86
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-24 14:36:07 -0400

    Adding flash alerts and notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:email])
+  	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to restaurants_url, notice: "Logged in!"
+  		redirect_to restaurants_url, :notice => "Logged in!"
   	else
       flash.now[:alert] = "Invalid email or password"
   		render "new"
