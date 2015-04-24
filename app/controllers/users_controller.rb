c2f15607862e8bf9898d7b9ebb73a81b53f9cda0
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-24 13:48:27 -0400

    Displaying validation errors on the user and restaurant forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,39 @@
 class UsersController < ApplicationController
 
-	def new
-  	@user = User.new
+  def show
+    @user = User.find(params[:id])
   end
 
-  def create
-  	@user = User.new(user_params)
+  def new
+    @user = User.new
+  end
 
-  	if @user.save
-  		redirect_to restaurants_url, notice: "Signed up!"
-  	else
-  		render "new"
-  	end
+def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to restaurants_url, notice: "Signed up!"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to restaurants_path
+    else
+      render :edit
+    end
   end
 
   private
   def user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, :category, :website, :capacity, presence: true
+	validates :capacity, numericality: {only_integer: true}
 end
