26203b046165c954c337d91d777c24c9c16e097b
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-24 12:57:38 -0400

    fixed bugs in user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,8 +11,12 @@ class RestaurantsController < ApplicationController
   	@restaurant = Restaurant.new
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def create
-  	@restaurant = Restaurant.new
+  	@restaurant = Restaurant.new(restaurant_params)
 
   	if @restaurant.save
   		redirect_to restaurants_url
@@ -21,12 +25,9 @@ class RestaurantsController < ApplicationController
   	end
  	end
 
-  def edit
-  	@restaurant = Restaurant.find(params[:id])
-  end
-
   def update
-  	@restaurant = Restaurant.find(params(:id))
+  	@restaurant = Restaurant.find(params[:id])
+
   	if @restaurant.update_attributes(restaurant_params)
   		redirect_to restaurant_path(@restaurant)
   	else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-  def new
+
+	def new
   	@user = User.new
   end
 
   def create
   	@user = User.new(user_params)
+
   	if @user.save
   		redirect_to restaurants_url, notice: "Signed up!"
   	else
@@ -16,5 +18,4 @@ class UsersController < ApplicationController
   def user_params
   	params.require(:user).permit(:email, :password, :password_confirmation)
   end
-
 end
